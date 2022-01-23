0dc889e6d60fe3162cba841b776b2495edd39805
Audrey Cornaire <audrey.cornaire@gmail.com>
2022-01-23 21:12:51 +0100

    Update kNN method

--- src/recommendation_system.py
+++ src/recommendation_system.py
@@ -1,6 +1,9 @@
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
 from skimage import io
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -43,23 +46,58 @@ class Recommendation():
 
         return track_features_copy
 
-    def generate_playlist_vector(self, track_features, playlist):
+    def generate_playlist_nonplaylist(self, track_features, playlist):
     
         features_playlist = track_features[track_features.index.isin(playlist.index.values)]
-        spotify_features_nonplaylist = track_features[~track_features.index.isin(playlist.index.values)]
+        features_nonplaylist = track_features[~track_features.index.isin(playlist.index.values)]
         
-        return features_playlist.sum(axis = 0), spotify_features_nonplaylist
+        return features_playlist, features_nonplaylist
 
         
-    def generate_recommendation(self, track_features, playlist, nonplaylist, track_infos):
+    def get_cosine_similarity(self, features_playlist, features_nonplaylist, track_infos):
 
-        non_playlist = track_features[track_features.index.isin(nonplaylist.index.values)]
-        non_playlist['sim'] = cosine_similarity(nonplaylist.values, playlist.values.reshape(1, -1))[:,0]
+        features_playlist_copy = features_playlist.copy()
+        features_playlist_copy.sum(axis = 0)
 
-        recommendation = non_playlist.sort_values('sim',ascending = False).head(10)
-        recommendation = recommendation.merge(track_infos, left_index=True, right_index=True)
+        features_nonplaylist_copy = features_nonplaylist.copy()
+
+        # tfidf_vectorizer = TfidfVectorizer()
+        # features_nonplaylist_copy = tfidf_vectorizer.fit_transform(features_nonplaylist_copy)
+        # features_playlist_copy = tfidf_vectorizer.transform(features_playlist_copy)
         
-        return recommendation
+        features_nonplaylist_copy['sim'] = features_nonplaylist_copy.apply(lambda x: cosine_similarity(x.values.reshape((1, -1)), features_playlist_copy.values), axis=1)
+        features_nonplaylist_copy['sim_mean'] = features_nonplaylist_copy.apply(lambda x: np.mean(x['sim'][0]), axis=1)
+        output = features_nonplaylist_copy[['sim', 'sim_mean']].merge(track_infos, left_index=True, right_index=True)
+
+        return output
+
+    def get_recommendations(self, playlist, nonplaylist, track_features, track_titles):
+
+        neighbors = NearestNeighbors(n_neighbors=5, algorithm='kd_tree').fit(nonplaylist)
+
+        distances, predictions = neighbors.kneighbors(playlist, 3, return_distance=True)
+
+        keys = {'key':[], 'distance':[], 'weight':[]}
+
+        for prediction_list, distance_list in zip(predictions, distances):
+            for prediction, distance in zip(prediction_list, distance_list):
+                index = track_features.iloc[prediction].name
+                if index not in keys['key']:
+                    keys['key'].append(index)
+                    keys['distance'].append(distance)
+                    keys['weight'].append(1)
+                else : 
+                    index_in_list = keys['key'].index(index)
+                    keys['weight'][index_in_list] += 1
+                    if distance < keys['distance'][index_in_list]: 
+                        keys['distance'][index_in_list] = distance
+
+                        # /!\ UTILISER POIDS
+
+        keys_df = pd.DataFrame.from_dict(keys).reset_index(drop=True)
+        keys_df = keys_df.sort_values(['distance'], ascending=True).head(10)
+
+        return keys_df.merge(track_titles, left_on='key', right_index=True).set_index('key'), track_features.loc[keys_df.key]
 
     def visualize_cover(self, playlist_df):
         temp = playlist_df['album_img'].values
